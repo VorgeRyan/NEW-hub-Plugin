c717487a96b50d3e6e7714549ec5f77f4cb79740
VorgeRyan <67753160+VorgeRyan@users.noreply.github.com>
2020-11-13 20:16:46 -0500

    Update HubPlugin.php

--- HubPlugin.php
+++ HubPlugin.php
@@ -1,9 +1,15 @@
 <?php
 namespace VorgeRyan\Hub;
 use pocketmine\plugin\PluginBase;
-use pocketmine\{Server, Player};
-use pocketmine\command\{Command, CommandSender};
+
+use pocketmine\Server;
+use pocketmine\Player;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
 use pocketmine\event\Listener;
+
 class Main extends PluginBase implements Listener{
 	
 	public function onEnable(){
